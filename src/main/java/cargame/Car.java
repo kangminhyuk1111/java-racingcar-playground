51f3d24f706d5daaf780c535c27d4470be75ce54
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-10-14 16:00:22 +0900

    feat: car test 작성 - instance 생성시 exception 처리

--- src/main/java/cargame/Car.java
+++ src/main/java/cargame/Car.java
@@ -6,6 +6,10 @@ public class Car {
     private int position;
 
     public Car(final String name) {
+        if (name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException("글자수는 0자리이상 5자리 이하로 제한합니다");
+        }
+
         this.name = name;
         this.position = 0;
     }
--- src/test/java/cargame/CarTest.java
+++ src/test/java/cargame/CarTest.java
@@ -7,15 +7,21 @@ import static org.assertj.core.api.Assertions.*;
 public class CarTest {
 
     final String name = "k5";
+    final String wrongName = "hyundai";
 
     @Test
-    void createCar() {
+    void 차량_생성() {
         final Car car = new Car(name);
         assertThat(car.getName()).isEqualTo(name);
     }
 
     @Test
-    void carMove() {
+    void 차량_생성시_글자수_제한_오류() {
+        assertThatThrownBy(() -> new Car(wrongName)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 차량_이동() {
         final Car car = new Car(name);
         car.move();
         assertThat(car.getPosition()).isEqualTo(1);
